7a61cd08cb7100611bbcf53e2f52ad68db2bd0e2
Beisenbek Baisakov <bbaisakov@dar.kz>
2026-04-20 09:52:06 +0500

    upd

---
+++ weeek 14/snake.py
@@ -0,0 +1,54 @@
+import pygame
+
+from food import Food
+from worm import Worm
+
+tile_width = 20
+
+def create_background(screen, width, height):
+        colors = [(255, 255, 255), (212, 212, 212)]
+        y = 0
+        while y < height:
+                x = 0
+                while x < width:
+                        row = y // tile_width
+                        col = x // tile_width
+                        pygame.draw.rect(screen, colors[(row + col) % 2],pygame.Rect(x, y, tile_width, tile_width))
+                        x += tile_width
+                y += tile_width
+
+done = False
+
+pygame.init()
+screen = pygame.display.set_mode((400, 300))
+clock = pygame.time.Clock()
+
+worm = Worm(tile_width)
+food = Food(tile_width)
+
+while not done:
+        # Event filtering
+        filtered_events = []
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                    done = True
+            else:
+                filtered_events.append(event)
+
+
+        create_background(screen, 400, 300)
+
+        pos = food.can_eat(worm.points[0])
+        
+        if(pos != None):
+            worm.increase(pos)
+            food = Food(tile_width)
+       
+
+        worm.process_input(filtered_events)
+        worm.move()
+        worm.draw(screen)
+        food.draw(screen)
+        
+        pygame.display.flip()
+        clock.tick(5)
